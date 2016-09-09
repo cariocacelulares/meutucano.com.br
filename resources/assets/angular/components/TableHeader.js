c5761dd4d3cc9cf327c4c616c2256b1e017674a2
Cleiton Souza <cleiton7souza@gmail.com>
2016-09-09 14:50:50 -0300

    fix endereco detalhe cliente

--- resources/assets/angular/components/TableHeader.js
+++ resources/assets/angular/components/TableHeader.js
@@ -17,8 +17,6 @@
                     this.name = name;
                     this.vm   = vm;
 
-                    console.log(this.vm);
-
                     if (!$localStorage.pagination) $localStorage.pagination = {};
 
                     if (!$localStorage.pagination.hasOwnProperty(this.name)) {
