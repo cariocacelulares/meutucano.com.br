77032c33a33a9ed895b26186a5d9539732501357
Cleiton Souza <cleiton7souza@gmail.com>
2016-10-11 14:27:54 -0300

    fix magento debug

--- app/Http/Controllers/Integracao/MagentoController.php
+++ app/Http/Controllers/Integracao/MagentoController.php
@@ -452,7 +452,7 @@ class MagentoController extends Controller implements Integracao
                 );
 
                 if (is_soap_fault($stock)) {
-                    Debug::log('Não foi possível atualizar o estoque no magento', [$stock]);
+                    Log::debug('Não foi possível atualizar o estoque no magento', [$stock]);
                     throw new \Exception('Produto inexistente no magento', 2);
                 }
 
