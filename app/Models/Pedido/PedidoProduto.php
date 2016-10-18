2ca268c4600b8246f07fee0be5b0f015facc2842
Diego Schell Fernandes <diego.schell.f@gmail.com>
2016-10-18 14:47:29 -0200

    Ajuste imei inspecao

--- app/Models/Pedido/PedidoProduto.php
+++ app/Models/Pedido/PedidoProduto.php
@@ -51,87 +51,87 @@ class PedidoProduto extends \Eloquent
                 if ((int)$pedido->status !== 5) {
                     \Event::fire(new ProductDispach($pedidoProduto->produto, $pedidoProduto->quantidade));
                 }
+            }
 
-                // Inspecao tecnica
-                if ((int)$pedido->status !== 5 && $produto->estado == 1 && false) {
-                    // se o status do pedido for qualquer um diferente de cancelado, e for seminovo
+            // Inspecao tecnica
+            if ((int)$pedido->status !== 5 && $produto->estado == 1 && false) {
+                // se o status do pedido for qualquer um diferente de cancelado, e for seminovo
 
-                    // se o pedido nao tiver imei
-                    if (!$pedidoProduto->imei) {
-                        $inspecao = InspecaoTecnica
-                            ::leftJoin('pedido_produtos', 'pedido_produtos.imei', '=', 'inspecao_tecnica.imei')
-                            ->where('inspecao_tecnica.produto_sku', '=', $produto->sku)
-                            ->whereNull('pedido_produtos.imei')
-                            ->whereNotNull('inspecao_tecnica.imei')
-                            ->orderBy('created_at', 'ASC')
-                            ->get(['inspecao_tecnica.*'])
-                            ->first();
-                    } else {
-                        // se tiver imei
-                        $inspecao = InspecaoTecnica::where('imei', '=', $pedidoProduto->imei)->orderBy('created_at', 'ASC')->first();
-                    }
-
-                    // se existe um imei do produto revisado, aguardando um pedido
-                    if ($inspecao) {
-                        // associado este pedido produto com a inspecao pronta
-                        if (!$pedidoProduto->imei) {
-                            $pedidoProduto->imei = $inspecao->imei;
-                            $pedidoProduto->save();
-                        }
-
-                        $inspecao->pedido_produtos_id = $pedidoProduto->id;
-                        $inspecao->save();
-                    } else {
-                        // se não existe
-                        $inspecao = InspecaoTecnica::create([
-                            'produto_sku' => $produto->sku,
-                            'pedido_produtos_id' => $pedidoProduto->id
-                        ]);
-                    }
-                }
-                $produto = $pedidoProduto->produto;
-
-                if ((int)$pedido->status !== 5) {
-                    \Log::debug('pedido produto', [$pedidoProduto->produto, $pedidoProduto->quantidade]);
-                    \Event::fire(new ProductDispach($pedidoProduto->produto, $pedidoProduto->quantidade));
+                // se o pedido nao tiver imei
+                if (!$pedidoProduto->imei) {
+                    $inspecao = InspecaoTecnica
+                        ::leftJoin('pedido_produtos', 'pedido_produtos.imei', '=', 'inspecao_tecnica.imei')
+                        ->where('inspecao_tecnica.produto_sku', '=', $produto->sku)
+                        ->whereNull('pedido_produtos.imei')
+                        ->whereNotNull('inspecao_tecnica.imei')
+                        ->orderBy('created_at', 'ASC')
+                        ->get(['inspecao_tecnica.*'])
+                        ->first();
+                } else {
+                    // se tiver imei
+                    $inspecao = InspecaoTecnica::where('imei', '=', $pedidoProduto->imei)->orderBy('created_at', 'ASC')->first();
                 }
 
-                // Inspecao tecnica
-                if ((int)$pedido->status !== 5 && $produto->estado == 1 && false) {
-                    // se o status do pedido for qualquer um diferente de cancelado, e for seminovo
-
-                    // se o pedido nao tiver imei
+                // se existe um imei do produto revisado, aguardando um pedido
+                if ($inspecao) {
+                    // associado este pedido produto com a inspecao pronta
                     if (!$pedidoProduto->imei) {
-                        $inspecao = InspecaoTecnica
-                            ::leftJoin('pedido_produtos', 'pedido_produtos.imei', '=', 'inspecao_tecnica.imei')
-                            ->where('inspecao_tecnica.produto_sku', '=', $produto->sku)
-                            ->whereNull('pedido_produtos.imei')
-                            ->whereNotNull('inspecao_tecnica.imei')
-                            ->orderBy('created_at', 'ASC')
-                            ->get(['inspecao_tecnica.*'])
-                            ->first();
-                    } else {
-                        // se tiver imei
-                        $inspecao = InspecaoTecnica::where('imei', '=', $pedidoProduto->imei)->orderBy('created_at', 'ASC')->first();
+                        $pedidoProduto->imei = $inspecao->imei;
+                        $pedidoProduto->save();
                     }
 
-                    // se existe um imei do produto revisado, aguardando um pedido
-                    if ($inspecao) {
-                        // associado este pedido produto com a inspecao pronta
-                        if (!$pedidoProduto->imei) {
-                            $pedidoProduto->imei = $inspecao->imei;
-                            $pedidoProduto->save();
-                        }
+                    $inspecao->pedido_produtos_id = $pedidoProduto->id;
+                    $inspecao->save();
+                } else {
+                    // se não existe
+                    $inspecao = InspecaoTecnica::create([
+                        'produto_sku' => $produto->sku,
+                        'pedido_produtos_id' => $pedidoProduto->id
+                    ]);
+                }
+            }
+            $produto = $pedidoProduto->produto;
 
-                        $inspecao->pedido_produtos_id = $pedidoProduto->id;
-                        $inspecao->save();
-                    } else {
-                        // se não existe
-                        $inspecao = InspecaoTecnica::create([
-                            'produto_sku' => $produto->sku,
-                            'pedido_produtos_id' => $pedidoProduto->id
-                        ]);
+            if ((int)$pedido->status !== 5) {
+                \Log::debug('pedido produto', [$pedidoProduto->produto, $pedidoProduto->quantidade]);
+                \Event::fire(new ProductDispach($pedidoProduto->produto, $pedidoProduto->quantidade));
+            }
+
+            // Inspecao tecnica
+            if ((int)$pedido->status !== 5 && $produto->estado == 1) {
+                // se o status do pedido for qualquer um diferente de cancelado, e for seminovo
+
+                // se o pedido nao tiver imei
+                if (!$pedidoProduto->imei) {
+                    $inspecao = InspecaoTecnica
+                        ::leftJoin('pedido_produtos', 'pedido_produtos.imei', '=', 'inspecao_tecnica.imei')
+                        ->where('inspecao_tecnica.produto_sku', '=', $produto->sku)
+                        ->whereNull('pedido_produtos.imei')
+                        ->whereNotNull('inspecao_tecnica.imei')
+                        ->orderBy('created_at', 'ASC')
+                        ->get(['inspecao_tecnica.*'])
+                        ->first();
+                } else {
+                    // se tiver imei
+                    $inspecao = InspecaoTecnica::where('imei', '=', $pedidoProduto->imei)->orderBy('created_at', 'ASC')->first();
+                }
+
+                // se existe um imei do produto revisado, aguardando um pedido
+                if ($inspecao) {
+                    // associado este pedido produto com a inspecao pronta
+                    if (!$pedidoProduto->imei) {
+                        $pedidoProduto->imei = $inspecao->imei;
+                        $pedidoProduto->save();
                     }
+
+                    $inspecao->pedido_produtos_id = $pedidoProduto->id;
+                    $inspecao->save();
+                } else {
+                    // se não existe
+                    $inspecao = InspecaoTecnica::create([
+                        'produto_sku' => $produto->sku,
+                        'pedido_produtos_id' => $pedidoProduto->id
+                    ]);
                 }
             }
         });
