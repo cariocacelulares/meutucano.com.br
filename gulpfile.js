44d35e3f5f80bce66531377cb68159d8035ffb83
Cleiton Souza <cleiton7souza@gmail.com>
2016-10-21 10:32:49 -0200

    gulp build on watch, gulp notify on error

--- gulpfile.js
+++ gulpfile.js
@@ -14,48 +14,67 @@ var gulpUtil     = require('gulp-util');
 var lib          = require('bower-files')();
 
 elixir.extend('customSass', function() {
-    new elixir.Task('customSass', function() {
+    var task = new elixir.Task('customSass', function() {
         return gulp.src(['resources/assets/sass/app.scss'])
             .pipe(sourcemaps.init())
-            .pipe(sass().on('error', sass.logError))
+            .pipe(sass().on('error', notify.onError(function(error) {
+                gulpUtil.log(error);
+                return error.message;
+            })))
             .pipe(rename('app.min.css'))
             .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
             .pipe(cleanCss())
             .pipe(sourcemaps.write())
             .pipe(gulp.dest('public/assets/css'))
             .pipe(notify('Sass compiled!'));
-    })
-    .watch('resources/assets/sass/**/*.scss');
+    });
+
+    task.run();
+    task.watch('resources/assets/sass/**/*.scss');
 });
 
 elixir.extend('angularMinify', function() {
-    new elixir.Task('angularMinify', function() {
+    var task = new elixir.Task('angularMinify', function() {
         return gulp.src("resources/assets/angular/**/*.js")
-            .pipe(ngmin().on('error', gulpUtil.log))
+            .pipe(ngmin().on('error', notify.onError(function(error) {
+                gulpUtil.log(error);
+                return error.message;
+            })))
             .pipe(concat('app.min.js'))
-            .pipe(uglify({mangle: false}).on('error', gulpUtil.log))
+            .pipe(uglify({mangle: false}).on('error', notify.onError(function(error) {
+                gulpUtil.log(error);
+                return error.message;
+            })))
             .pipe(gulp.dest('public/assets/js/'))
             .pipe(notify('JS compiled!'));
-    })
-    .watch('resources/assets/angular/**/*.js');
+    });
+
+    task.run();
+    task.watch('resources/assets/angular/**/*.js');
 });
 
 elixir.extend('bowerJs', function() {
-    new elixir.Task('bowerJs', function() {
+    var task = new elixir.Task('bowerJs', function() {
         return gulp.src(lib.ext('js').files)
             .pipe(concat('lib.min.js'))
             .pipe(uglify())
-            .pipe(gulp.dest('public/assets/js'));
+            .pipe(gulp.dest('public/assets/js'))
+            .pipe(notify('Bower JS compiled!'));
     });
+
+    task.run();
 });
 
 elixir.extend('bowerCss', function() {
-    new elixir.Task('bowerCss', function() {
+    var task = new elixir.Task('bowerCss', function() {
         return gulp.src(lib.ext('css').files)
             .pipe(concat('lib.min.css'))
             .pipe(cleanCss())
-            .pipe(gulp.dest('public/assets/css'));
+            .pipe(gulp.dest('public/assets/css'))
+            .pipe(notify('Bower CSS compiled!'));
     });
+
+    task.run();
 });
 
 elixir(function(mix) {
