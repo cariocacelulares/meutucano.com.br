9de6ed879b294710a8a3d2a221f6b1166a83d114
Cleiton Souza <cleiton7souza@gmail.com>
2016-11-03 01:40:34 -0200

    gamification - ranking: historico, countdown, refresh

--- resources/assets/angular/models/Gamification/Gamification.js
+++ resources/assets/angular/models/Gamification/Gamification.js
@@ -13,8 +13,18 @@
                     return Restangular.one(baseUrl + '/perfil', usuario_id || null).customGET();
                 },
 
-                ranking: function() {
-                    return Restangular.one(baseUrl + '/ranking').customGET();
+                ranking: function(mes) {
+                    var string = '';
+
+                    if (typeof mes !== 'undefined') {
+                        string = '?mes=' + mes;
+                    }
+
+                    return Restangular.one(baseUrl + '/ranking' + string).customGET();
+                },
+
+                rankInfo: function() {
+                    return Restangular.one(baseUrl + '/rank-info').customGET();
                 },
 
                 avatar: function(gamification_id, avatar) {
