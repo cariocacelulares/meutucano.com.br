ff0fa4737ae8e168d1b17e0e8fd6e4b261559180
Diego Schell Fernandes <diego.schell.f@gmail.com>
2016-12-13 10:32:58 -0200

    Configuração de banco de dados na DigitalOcean

--- config/database.php
+++ config/database.php
@@ -7,17 +7,17 @@ return [
     'connections' => [
 
         'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'meutucano'),
-            'username' => env('DB_USERNAME', 'dev'),
-            'password' => env('DB_PASSWORD', 'carioca5102'),
-            'charset' => 'utf8',
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'port'      => env('DB_PORT', '3306'),
+            'database'  => env('DB_DATABASE', 'meutucano'),
+            'username'  => env('DB_USERNAME', 'carioca'),
+            'password'  => env('DB_PASSWORD', '#@carioca2016'),
+            'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => false,
-            'engine' => null,
+            'prefix'    => '',
+            'strict'    => false,
+            'engine'    => null,
         ],
 
         'mongodb' => [
