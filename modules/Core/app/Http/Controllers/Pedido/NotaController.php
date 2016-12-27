0df7ddfb23cd9bc949808dfb129ee9d1f77caebc
Cleiton Souza <cleiton7souza@gmail.com>
2016-12-27 15:07:12 -0200

    component e helper para validacao de form

--- modules/Core/app/Http/Controllers/Pedido/NotaController.php
+++ modules/Core/app/Http/Controllers/Pedido/NotaController.php
@@ -103,7 +103,7 @@ class NotaController extends Controller
                 }
             }
         } catch (\Exception $e) {
-            Log::warning('Falha ao tentar enviar um e-mail de venda', [$id]);
+            \Log::warning('Falha ao tentar enviar um e-mail de venda', [$id]);
             return $this->clientErrorResponse();
         }
 
---
+++ resources/assets/angular/components/ValidationErrors.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('MeuTucano')
+        .component('validation', {
+            bindings: {
+                errors: '='
+            },
+            template:
+                '<div ng-if="$ctrl.errors.length" class="row">' +
+                    '<div class="col-md-12 m-b-10">' +
+                        '<div class="alert alert-warning">' +
+                            '<ul class="p-l-20 m-t-0 m-b-0">' +
+                                '<li ng-repeat="error in $ctrl.errors track by $index">{{ error }}</li>' +
+                            '</ul>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>'
+        });
+
+})();
