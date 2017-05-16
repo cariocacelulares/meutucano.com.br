88267698bc6e1a932dda06c79a1ad614b82335d2
Diego Schell Fernandes <diego.schell.f@gmail.com>
2017-05-16 14:15:02 -0300

    Remove arquivos antigos

--- resources/views/emails/danfe.blade.php
+++
@@ -1,6 +0,0 @@
-Olá,
-<br><br>
-Segue em anexo uma cópia da nota fiscal do seu pedido. Qualquer dúvida entre em contato respondendo esse e-mail.
-<br><br>
-Att.<br>
-Equipe Carioca Celulares Online.
