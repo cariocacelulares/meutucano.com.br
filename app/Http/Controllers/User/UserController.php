b50e67f182c25c1b8166a88f7092ebad07393370
Diego Schell Fernandes <diego.schell.f@gmail.com>
2017-05-18 18:44:31 -0300

    Roles e permissões na request do usuário

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -1,6 +1,8 @@
 <?php namespace App\Http\Controllers\User;
 
 use App\Models\User\User;
+use App\Models\User\Role;
+use App\Models\User\Permission;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserRequest as Request;
 
@@ -15,7 +17,7 @@ class UserController extends Controller
         $this->middleware('permission:user_update', ['only' => ['update']]);
         $this->middleware('permission:user_delete', ['only' => ['destroy']]);
 
-        $this->middleware('convertJson', ['only' => ['index']]);
+        $this->middleware('convertJson', ['only' => ['index', 'store']]);
     }
 
     /**
@@ -78,6 +80,8 @@ class UserController extends Controller
         try {
             $data = User::create($request->all());
 
+            $this->syncRolesAndPermissions($data, $request);
+
             return createdResponse($data);
         } catch (\Exception $exception) {
             \Log::error(logMessage($exception, 'Erro ao salvar recurso'));
@@ -99,6 +103,8 @@ class UserController extends Controller
             $data->fill($request->all());
             $data->save();
 
+            $this->syncRolesAndPermissions($data, $request);
+
             return showResponse($data);
         } catch (\Exception $exception) {
             \Log::error(logMessage($exception, 'Erro ao atualizar recurso'));
@@ -106,6 +112,28 @@ class UserController extends Controller
             return clientErrorResponse([
                 'exception' => '[' . $exception->getLine() . '] ' . $exception->getMessage()
             ]);
+        }
+    }
+
+    /**
+     * Update roles and permissions from user
+     *
+     * @param  User $user
+     * @param  Request $request
+     * @return void
+     */
+    private function syncRoutesAndPermissions($user, $request)
+    {
+        if ($roleIds = $request->input('roles')) {
+            $roles = Role::whereIn('id', $roleIds)->get();
+            $user->roles()->delete();
+            $user->roles()->saveMany($roles);
+        }
+
+        if ($permissionIds = $request->input('permissions')) {
+            $permissions = Permission::whereIn('id', $permissionIds)->get();
+            $user->perms()->delete();
+            $user->perms()->saveMany($permissions);
         }
     }
 
